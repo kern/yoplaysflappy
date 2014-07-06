8f3eb9283d313c220491546324b7409ab154e491
angel <sayangel@gmail.com>
2014-07-06 12:24:18 -0400

    added persistent stats in case of crash

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
+var jf = require('jsonfile');
 
 var phantom = require('phantom');
+
+var statsfile = './stats.json';
 
 app.use(express.static(process.cwd() + '/public'));
 
@@ -21,15 +24,28 @@ http.listen(process.env.PORT || 3000, function(){
 
     phantom.create(function(ph) {
         ph.createPage(function(page) {
-            var yos = 0;
-            var lastYo = "";
-            var highScore = 0;
+            var stats = {};
+            //var yos = 0;
+            //var lastYo = "";
+            //var highScore = 0;
+
+            jf.readFile(statsfile, function(err, obj) {
+              stats.yos = obj.yos;
+              stats.lastYo = obj.lastYo;
+              stats.highScore = obj.highScore;
+              console.log(obj.yos);
+              console.log(obj.lastYo);
+              console.log(obj.highscore);
+            });
 
             app.get('/yo', function(req, res) {
-                yos++;
-                lastYo = req.query.username;
+                stats.yos++;
+                stats.lastYo = req.query.username;
                 page.sendEvent("keypress", 32);
                 res.end();
+                jf.writeFile(statsfile, stats, function(err) {
+                  console.log(err);
+                })
             });
 
             page.open("http://www.yoplaysflappy.com/render", function(status) {
@@ -40,14 +56,17 @@ http.listen(process.env.PORT || 3000, function(){
                             state: document.getElementById("testCanvas").toDataURL("image/jpeg", 0.1)
                         };
                     }, function(result) {
-                        if (result.score > highScore) {
-                            highScore = result.score;
+                        if (result.score > stats.highScore) {
+                            stats.highScore = result.score;
+                            jf.writeFile(statsfile, stats, function(err) {
+                              console.log(err);
+                            })
                         }
 
                         io.emit('update', {
-                            highScore: highScore,
-                            lastYo: lastYo,
-                            yos: yos,
+                            highScore: stats.highScore,
+                            lastYo: stats.lastYo,
+                            yos: stats.yos,
                             state: result.state
                         });
                     });
