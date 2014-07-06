cc79d66ade4a30f4a06e328759aa0818b30cfa6a
angel <sayangel@gmail.com>
2014-07-06 15:49:27 -0400

    added mongoose.

--- index.js
+++ index.js
@@ -3,11 +3,38 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-var jf = require('jsonfile');
+var mongoose = require ("mongoose");
 
 var phantom = require('phantom');
 
-var statsfile = './stats.json';
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/StatsDB';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+var statsSchema = new mongoose.Schema({
+  yos: {type: Number, min: 0},
+  lastYo: String,
+  highScore: {type: Number, min: 0}
+});
+
+var gameStats = mongoose.model('gameStats', statsSchema);
+
+var stats = new gameStats ({
+  yos: 315,
+  lastYo: "SAYANGEL",
+  highScore: 14
+});
+
+stats.save(function (err) {if (err) console.log ('Error on save!')});
 
 app.use(express.static(process.cwd() + '/public'));
 
@@ -24,15 +51,18 @@ http.listen(process.env.PORT || 3000, function(){
 
     phantom.create(function(ph) {
         ph.createPage(function(page) {
-            var stats = {};
-            //var yos = 0;
-            //var lastYo = "";
-            //var highScore = 0;
 
-            jf.readFile(statsfile, function(err, obj) {
-              stats.yos = obj.yos;
-              stats.lastYo = obj.lastYo;
-              stats.highScore = obj.highScore;
+            var stats = {};
+            stats.yos = 0;
+            stats.lastYo = "";
+            stats.highScore = 0;
+
+            gameStats.findOne({}, function(err, doc) {
+              if (err) return console.error(err);
+              //console.dir(doc);
+              stats.yos = doc.yos;
+              stats.lastYo = doc.lastYo;
+              stats.highScore = doc.highScore;
             });
 
             app.get('/yo', function(req, res) {
@@ -40,11 +70,13 @@ http.listen(process.env.PORT || 3000, function(){
                 stats.lastYo = req.query.username;
                 page.sendEvent("keypress", 32);
                 res.end();
-                jf.writeFile(statsfile, stats, function(err) {
-                  if(err){
-                    console.log(err);
-                  };
-                })
+                gameStats.findOne({}, function(err, doc) {
+                  if (err) return console.error(err);
+                  //console.dir(doc);
+                  doc.yos = stats.yos;
+                  doc.lastYo = stats.lastYo;
+                  doc.save(function (err) {if (err) console.log ('Error on save!')});
+                });
             });
 
             page.open("http://www.yoplaysflappy.com/render", function(status) {
@@ -57,11 +89,12 @@ http.listen(process.env.PORT || 3000, function(){
                     }, function(result) {
                         if (result.score > stats.highScore) {
                             stats.highScore = result.score;
-                            jf.writeFile(statsfile, stats, function(err) {
-                              if(err){
-                                console.log(err);
-                              };
-                            })
+                            gameStats.findOne({}, function(err, doc) {
+                              if (err) return console.error(err);
+                              //console.dir(doc);
+                              doc.highScore = stats.highScore;
+                              doc.save(function (err) {if (err) console.log ('Error on save!')});
+                            });
                         }
 
                         io.emit('update', {
